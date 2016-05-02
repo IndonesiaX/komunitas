bde6e9088e097b5661d8b0f3e76f5f4e0d4cb4ab
ariestiyansyah <ariestiyansyah.rizky@gmail.com>
2016-05-02 16:58:22 +0700

    Alpha-001

---
+++ js/style.js
@@ -0,0 +1,65 @@
+// MainNav
+$(window).resize(function() {
+  var more = document.getElementById("js-navigation-more");
+  var windowWidth = $(window).width();
+  var moreLeftSideToPageLeftSide = $(more).offset().left;
+  var moreLeftSideToPageRightSide = windowWidth - moreLeftSideToPageLeftSide;
+
+  if (moreLeftSideToPageRightSide < 330) {
+    $("#js-navigation-more .submenu .submenu").removeClass("fly-out-right");
+    $("#js-navigation-more .submenu .submenu").addClass("fly-out-left");
+  }
+
+  if (moreLeftSideToPageRightSide > 330) {
+    $("#js-navigation-more .submenu .submenu").removeClass("fly-out-left");
+    $("#js-navigation-more .submenu .submenu").addClass("fly-out-right");
+  }
+});
+
+$(document).ready(function() {
+  //Menu Mobile
+  var menuToggle = $("#js-mobile-menu").unbind();
+  $("#js-navigation-menu").removeClass("show");
+  menuToggle.on("click", function(e) {
+    e.preventDefault();
+    $("#js-navigation-menu").slideToggle(function(){
+      if($("#js-navigation-menu").is(":hidden")) {
+        $("#js-navigation-menu").removeAttr("style");
+      }
+    });
+  });
+
+  //jQuery Grid Rotator
+  $( '#ri-grid' ).gridrotator( {
+            rows : 4,
+            columns : 12,
+            maxStep : 2,
+            interval : 2000,
+            w1024 : {
+              rows : 4,
+              columns : 9
+            },
+            w768 : {
+              rows : 5,
+              columns : 7
+            },
+            w480 : {
+              rows : 6,
+              columns : 5
+            },
+            w320 : {
+              rows : 7,
+              columns : 4
+            },
+            w240 : {
+              rows : 7,
+              columns : 3
+            },
+          } );
+
+}); 
+
+
+
+
+
